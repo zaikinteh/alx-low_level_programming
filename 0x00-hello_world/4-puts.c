281603cc15af7a317a744ea70b25a43bcc73da07
zaikinteh <zainabkinteh22@gmail.com>
2023-09-07 15:12:23 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,9 @@
  * the program is going to print "Programming is like building a multilingual puzzle"
  *Return: 0
  */
+
  int main(void)
-	{
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
-	}
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
