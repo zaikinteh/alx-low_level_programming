2bc2062e41a273dd8853a138ac7c0128bc0f8a44
zaikinteh <zainabkinteh22@gmail.com>
2023-09-08 16:19:50 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,12 +3,14 @@
 #include <time.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 	int last;
+	/* your code goes there */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
