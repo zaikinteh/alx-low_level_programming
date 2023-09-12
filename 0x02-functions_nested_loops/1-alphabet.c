bd00d9efd94cb81fc0d19e301c3d7765746c0701
zaikinteh <zainabkinteh22@gmail.com>
2023-09-12 15:45:33 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,8 @@
 
 void print_alphabet(void)
 {
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++);
-	_putchar(ch);
-	_putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++);
+_putchar(ch);
+_putchar('\n');
 }
