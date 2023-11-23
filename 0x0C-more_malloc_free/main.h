ff6c03a7364995f3bc03a8f3d745d8fd06d3db68
zaikinteh <zainabkinteh22@gmail.com>
2023-11-23 05:17:53 -0800

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,9 +7,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _puts(char *str);
-int _atoi(const char *s);
-void print_int (unsigned long int n);
-int main(int argc char const *argv[])
+void(errors)(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
